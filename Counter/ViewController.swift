384c30e5ddd491902d4528911099c00adb4a559e
Andrei Kigai <avkigai@gmail.com>
2024-11-04 14:41:00 +0300

    Усложнённая задача: добавил кнопку уменьшения счетчика, кнопку сброса и лог событий

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -3,8 +3,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var counter: UILabel!
     @IBOutlet weak var counterPlusButton: UIButton!
+    @IBOutlet weak var counterMinusButton: UIButton!
+    @IBOutlet weak var logTextView: UITextView!
     
-    private var counterValue: Int = 0 {
+    private var logger: Logger!
+    
+    private var counterValue = 0 {
         didSet {
             counter.text = "Значение счётчика: \(counterValue)"
         }
@@ -12,13 +16,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.logTextView.isEditable = false
+        
+        self.logger = Logger(targetTextView: self.logTextView)
     }
     
     @IBAction func incrementCounter(_ sender: Any) {
         let (incremented, isOverflow) = counterValue.addingReportingOverflow(1)
         if !isOverflow {
             counterValue = incremented
+            logger.log(.increment(val: 1))
         }
+    }
+    
+    @IBAction func decrementCounter(_ sender: Any) {
+        if counterValue > 0 {
+            counterValue -= 1
+            logger.log(.decrement(val: 1))
+        } else {
+            logger.log(.decrementBelowZero)
+        }
+    }
+    
+    @IBAction func resetCounter(_ sender: Any) {
+        counterValue = 0
+        logger.log(.reset)
     }
 }
 
