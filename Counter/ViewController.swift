2bc6cb873c2c1f55b53daaf765a45b6b7640d8ab
Andrei Kigai <avkigai@gmail.com>
2024-11-04 13:24:40 +0300

    Базовая задача: реализовал базовый счетчик с одной кнопкой

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,14 +1,24 @@
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var counterPlusButton: UIButton!
+    
+    private var counterValue: Int = 0 {
+        didSet {
+            counter.text = "Значение счётчика: \(counterValue)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func incrementCounter(_ sender: Any) {
+        let (incremented, isOverflow) = counterValue.addingReportingOverflow(1)
+        if !isOverflow {
+            counterValue = incremented
+        }
+    }
 }
 
