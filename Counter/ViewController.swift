853af07f37a5753df4118c17b1294f2bef63f60b
Andrei Kigai <avkigai@gmail.com>
2024-11-07 14:41:01 +0300

    Исправление замечаний

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,47 +1,47 @@
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var counter: UILabel!
-    @IBOutlet weak var counterPlusButton: UIButton!
-    @IBOutlet weak var counterMinusButton: UIButton!
-    @IBOutlet weak var logTextView: UITextView!
+final class ViewController: UIViewController {
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterPlusButton: UIButton!
+    @IBOutlet private weak var counterMinusButton: UIButton!
+    @IBOutlet private weak var logTextView: UITextView!
     
-    private var logger: Logger!
+    private var logger: Logger?
     
     private var counterValue = 0 {
         didSet {
-            counter.text = "Значение счётчика: \(counterValue)"
+            counterLabel.text = "Значение счётчика: \(counterValue)"
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.logTextView.isEditable = false
+        logTextView.isEditable = false
         
-        self.logger = Logger(targetTextView: self.logTextView)
+        logger = Logger(targetTextView: logTextView)
     }
     
-    @IBAction func incrementCounter(_ sender: Any) {
+    @IBAction private func incrementCounterButtonTapped(_ sender: Any) {
         let (incremented, isOverflow) = counterValue.addingReportingOverflow(1)
         if !isOverflow {
             counterValue = incremented
-            logger.log(.increment(val: 1))
+            logger?.log(.increment(val: 1))
         }
     }
     
-    @IBAction func decrementCounter(_ sender: Any) {
+    @IBAction private func decrementCounterButtonTapped(_ sender: Any) {
         if counterValue > 0 {
             counterValue -= 1
-            logger.log(.decrement(val: 1))
+            logger?.log(.decrement(val: 1))
         } else {
-            logger.log(.decrementBelowZero)
+            logger?.log(.decrementBelowZero)
         }
     }
     
-    @IBAction func resetCounter(_ sender: Any) {
+    @IBAction private func resetCounterButtonTapped(_ sender: Any) {
         counterValue = 0
-        logger.log(.reset)
+        logger?.log(.reset)
     }
 }
 
